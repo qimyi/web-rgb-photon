003ce0e244e8edc5ce93258abf68ccfe0ee83991
qlee <qlee@dius.com.au>
2017-04-30 00:46:46 +1000

    Added adjust brightness

--- src/Web-RGB.ino
+++ src/Web-RGB.ino
@@ -7,6 +7,7 @@
 
  // This #include statement was automatically added by the Particle IDE.
  #include <neopixel.h>
+ #include <math.h>
 
  #include "Particle.h"
 
@@ -19,6 +20,9 @@
 
  Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
  uint8_t colors[3];
+ unsigned long timeElapsed;
+
+uint8_t leds[PIXEL_COUNT][4];
 
  void setup() {
    Particle.function("led",ledToggle);
@@ -31,7 +35,28 @@
  }
 
  void loop() {
+   double angle = 2 * M_PI * 0.5 * millis() / 1000;
+   /*Serial.print("Time: ");*/
+   timeElapsed = millis();
+   //prints time since program started
+   /*Serial.print(timeElapsed);*/
+   /*Serial.print(" sin(t): ");*/
+   int brightness = (int) (255 * (sin(angle) + 1) / 2);
+   /*Serial.println(brightness);*/
 
+   uint16_t j;
+   for(j=0; j<strip.numPixels(); j++) {
+     strip.setColorDimmed(j, leds[j][0], leds[j][1], leds[j][2], brightness);
+     /*leds[j][0] = colors[0];
+     leds[j][1] = colors[1];
+     leds[j][2] = colors[2];
+     leds[j][3] = 255;*/
+
+   }
+   /*strip.setBrightness(brightness);*/
+   strip.show();
+   // wait a second so as not to send massive amounts of data
+   delay(16);
  }
 
  void getRGBArrayFromString(String rgb) {
@@ -52,6 +77,11 @@
    uint16_t j;
    for(j=0; j<strip.numPixels(); j++) {
      strip.setPixelColor(j, colors[0], colors[1], colors[2]);
+     leds[j][0] = colors[0];
+     leds[j][1] = colors[1];
+     leds[j][2] = colors[2];
+     leds[j][3] = 255;
+
    }
    strip.show();
 
@@ -78,6 +108,9 @@
    uint16_t ledIndex = parameter.substring(0, splitIndex).toInt();
    getRGBArrayFromString(parameter.substring(splitIndex + 1));
    strip.setPixelColor(ledIndex, colors[0], colors[1], colors[2]);
+   leds[ledIndex][0] = colors[0];
+   leds[ledIndex][1] = colors[1];
+   leds[ledIndex][2] = colors[2];
    strip.show();
    return 0;
  }
